54da23043e1fb20afbb72a0397ade926e593c1d6
Mikhael Dionio <mikhaeldionio@gmail.com>
2018-11-18 15:18:25 +0800

    redid installation using 'react-native init'

--- Babana/ios/babanaTests/babanaTests.m
+++ Babana/ios/babanaTests/babanaTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface babanaTests : XCTestCase
+@interface BabanaTests : XCTestCase
 
 @end
 
-@implementation babanaTests
+@implementation BabanaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
