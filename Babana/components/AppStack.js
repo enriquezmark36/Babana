21ebf31c5e645dac3da8d74f6aa84bc518e31fdb
Mark Enriquez <enriquezmark36@gmail.com>
2018-11-19 16:34:03 +0800

    set up react-navigation
    Go read the Babana/components/AppStack.js file on how to extend.
    
    Signed-off-by: Mark Enriquez <enriquezmark36@gmail.com>

---
+++ Babana/components/AppStack.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import App from '../components/App'
+
+// Define your additional Screens/Pages here
+// You may write it something like this:
+// const AppScreens = {
+//   About: About,
+//   Settings: Settings
+// };
+const AppScreens = {
+};
+
+const routes = Object.keys(AppScreens)
+  .map(id => ({ id, item: AppScreens[id] }))
+  .reduce((acc, { id, item }) => {
+    const Comp = item;
+    const Screen = (props => <Comp {...props} />);
+
+    if (typeof Comp.navigationOptions === 'undefined')
+      Screen.navigationOptions = (props) => ({
+        header: null
+      });
+    else
+      Screen.navigationOptions = Comp.navigationOptions;
+
+    return {
+      ...acc,
+      [id]: { screen: Screen },
+    };
+  }, {});
+
+
+// Just for the picky, removes header by default
+// if navigationOptions is undefined
+function disableHeaderIfUndef(Comp) {
+  if (typeof Comp.navigationOptions === 'undefined')
+    Comp.navigationOptions = (props) => ({
+      header: null
+    });
+  return Comp;
+}
+
+const AppStack = createStackNavigator(
+  {
+    App: { screen: disableHeaderIfUndef(App)},
+    ...routes,
+  },
+)
+
+export default createAppContainer(AppStack);
