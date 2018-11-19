9dd607bbdb83332d9999b6ce7db09bba70b42c38
Mark Enriquez <enriquezmark36@gmail.com>
2018-11-19 11:20:11 +0800

    Babana: Port react-native-paper theme for nativebase use
    This would probably would break many objects' general look.
    On the other hand, components from nativebase would be look more normal.
    
    To me it looks like react-native-paper did have a cousin. lol.
    
    I'll have to migrate to nativebase's soon.
    
    Signed-off-by: Mark Enriquez <enriquezmark36@gmail.com>
    (cherry picked from commit f4cf4c4bfbdd232bde2d915ad22df20f89a5dff8)
    [Cross Ported to the master branch]
    Signed-off-by: Mark Enriquez <enriquezmark36@gmail.com>

--- Babana/index.js
+++ Babana/index.js
@@ -1,7 +1,23 @@
 /** @format */
 
+import React from 'react';
+
 import {AppRegistry} from 'react-native';
 import AppStack from './components/AppStack';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => AppStack);
+//native-base theme
+import getTheme from './native-base-theme/components';
+import banana from './native-base-theme/variables/material';
+import { StyleProvider } from 'native-base';
+
+
+function Main() {
+  return (
+    <StyleProvider style={getTheme(banana)}>
+      <AppStack />
+    </StyleProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
