22eba01bd2818a398b64ae90048018d6069e9d95
Mark Enriquez <enriquezmark36@gmail.com>
2018-11-19 15:13:59 +0800

    add 2 more libraries
    react-native-gesture-handler : react-navigation dependency; will fail without it
    react-native-contacts	     : for accessing contact/phonebook
    
    Signed-off-by: Mark Enriquez <enriquezmark36@gmail.com>

--- Babana/android/app/src/main/java/com/babana/MainApplication.java
+++ Babana/android/app/src/main/java/com/babana/MainApplication.java
@@ -3,6 +3,8 @@ package com.babana;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rt2zz.reactnativecontacts.ReactNativeContacts;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeContacts(),
+            new RNGestureHandlerPackage(),
             new VectorIconsPackage(),
             new MapsPackage()
       );
